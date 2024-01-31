c8a352656e0c4aa1385ac20dc13776e1dd7ff118
Eric Antunes <antunesv.eric@gmail.com>
2024-01-31 13:33:34 -0300

    created crud products

---
+++ src/main/java/com/eric/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,18 @@
+package com.eric.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.eric.farmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	
+	public List<Produto> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+	
+	public List<Produto> findByPrecoLessThanEqual(@Param("preco") double preco);
+    
+}
